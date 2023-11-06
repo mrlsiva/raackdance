be08c6b2ad6f51f3811a02c6d35330fc42b18d96
mrlsiva <54312287+mrlsiva@users.noreply.github.com>
2023-11-06 10:20:26 +0530

    css fixes

--- js/custom/custom.js
+++ js/custom/custom.js
@@ -66,6 +66,20 @@ $(document).ready(function () {
 			});
 		}
 	});
+	
+	/* Auto Play YT Video on section reach view port */
+	$(window).bind('scroll load', function () {		
+		if( $(document).find('.loadframeLazy').length ){ 
+			$(document).find('.loadframeLazy').each(function(index){
+				if ($(this).isInViewport() || $(this).isOutViewport() ) {						
+					if( $(this).attr('data-loadframe') == 'pause'){						
+						$(this).attr('src', $(this).attr('src')+'&autoplay=1');	
+						$(this).attr('data-loadframe', 'play');
+					}
+				}
+			});
+		}
+	});
 
 	//POPUP FORM 
 	$(".btn-hst-pop-modal").click(function() {        
@@ -78,6 +92,22 @@ $(document).ready(function () {
         $($(this).parent('.hst-pop-modal')).hide(400);
         return false;
     });
+
+	var splide = $('.splide');
+	if (splide.length) {
+	const splide = new Splide('.splide', {
+      type: 'fade',
+      rewind: true,
+      pagination: false,
+	  speed : 400,
+      interval : 5000,
+	  autoplay : true,
+	  arrows : false,
+	  drag : true
+    });
+
+    splide.mount();
+	}
 	
 	
 	// YOUTUBE BACKGROUND VIDEO FUNCTION	  
@@ -102,4 +132,67 @@ $(document).ready(function () {
 		});
 		return false;
 	});
+
+	var accord = $(".faq-accord");
+    	if(accord.length){
+    	  var i = 0; 
+    	  accord.each(function(){
+		var all_panels = $(this).find('.faq-ans').hide();  
+		
+		var all_titles = $(this).find('.faq-ques'); 
+		
+		$(this).find('.faq-ans.active').slideDown();    	
+		
+		all_titles.on("click", function() { 
+		
+		var acc_title = $(this);
+		var acc_parent = $(this).parents('.faq-container');
+		var acc_ans = acc_title.next(); 
+		
+			if (!acc_ans.hasClass('active')) {
+				acc_parent.find('.faq-ques').removeClass('active');  
+				acc_parent.find('.faq-ans').removeClass('active').slideUp();
+				
+				acc_title.addClass('active');  
+				acc_ans.addClass('active').slideDown(); 
+				} else {
+					all_panels.removeClass('active').slideUp();
+					all_titles.removeClass('active'); 
+				} 
+		});
+		});        
+	}
+	
+	/* Home Page - Header Slider Video */
+	if( $('#headerSlideVideo').length ){ 
+		var myvid = document.getElementById('headerSlideVideo');
+		/* var myvids = [
+		  "https://raackdance.com/videos/2.mp4", 
+		  "https://raackdance.com/videos/3.mp4",
+		  "https://raackdance.com/videos/4.mp4"
+		  ]; */
+		  
+		 var myvids = []; 
+		 var myvids2 = myvid.getAttribute('data-videos').split(",");
+		 myvids = myvids2;	 
+		  
+		var activeVideo = 0;
+
+		myvid.addEventListener('ended', function(e) {		
+		
+		 myvid.classList.remove("animate__fadeIn"); 
+		 myvid.classList.add("animate__fadeOut"); 
+		 
+		  // update the active video index
+		  activeVideo = (++activeVideo) % myvids.length;
+
+		  // update the video source and play
+		  myvid.src = myvids[activeVideo];	
+		 
+		  
+		  myvid.classList.remove("animate__fadeOut"); 
+		  myvid.classList.add("animate__fadeIn"); 
+		  myvid.play();
+		});
+	}
 });
--- js/vendor/slider/owl-slider.js
+++ js/vendor/slider/owl-slider.js
@@ -10,7 +10,7 @@ $(document).on('ready', function() {
         itemsTablet:[768,1],
 		itemsMobile:[575,1],
         pagination:false,
-        navigation:true,
+		navigation:true, 
         navigationText:["<svg xmlns='http://www.w3.org/2000/svg' height='1em' viewBox='0 0 320 512'><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d='M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z'/></svg>","<svg xmlns='http://www.w3.org/2000/svg' height='1em' viewBox='0 0 320 512'><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d='M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z'/></svg>"],
         autoPlay:true
 	  });
@@ -66,7 +66,9 @@ $(document).on('ready', function() {
         pagination:false,
         navigation:true,
 		navigationText:["<svg xmlns='http://www.w3.org/2000/svg' height='1em' viewBox='0 0 320 512'><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d='M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z'/></svg>","<svg xmlns='http://www.w3.org/2000/svg' height='1em' viewBox='0 0 320 512'><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d='M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z'/></svg>"],
-        autoPlay:true
+        autoPlay:true,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
 		  
 	  });
 	  
@@ -161,7 +163,7 @@ $(document).on('ready', function() {
 	  // Header CAROUSEL
 	  var team = $("#col-carousel");	 //my carousel is user defind
 	  team.owlCarousel({
-		items:3,
+		items:1,
         itemsDesktop:[1000,3],
         itemsDesktopSmall:[991,2],
         itemsTablet:[767,1],
