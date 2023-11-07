53d71e20d49b2e6c7a661b4ef0e3165720bf79bd
Arun <arunsubramaniyan208@gmail.com>
2023-11-07 00:04:45 -0800

    velachery corrected

--- js/custom/custom.js
+++ js/custom/custom.js
@@ -196,3 +196,15 @@ $(document).ready(function () {
 		});
 	}
 });
+
+/* view class scedule action */
+const question = document.querySelector('.faq .question');
+   const answer = document.querySelector('.faq .answer');
+
+   question.addEventListener('click', () => {
+       if (answer.style.display === 'none' || answer.style.display === '') {
+           answer.style.display = 'block';
+       } else {
+           answer.style.display = 'none';
+       }
+   });
